8885c5438f181ecba89272e813f8f5156335b986
M3RCURRRY <m3rcurrry1337@gmail.com>
2023-02-11 00:47:51 +0300

    Route for roulettte

--- src/routes/RouletteRoute/RouletteRoute.tsx
+++ src/routes/RouletteRoute/RouletteRoute.tsx
@@ -1,3 +1,4 @@
+import Chat from "../../components/Chat/Chat";
 import ModeSwitch from "../../components/ModeSwitch/ModeSwitch";
 import styles from "./RouletteRoute.module.scss";
 
@@ -5,6 +6,7 @@ const RouletteRoute = () => {
   return <div className={styles.container}>
     <div className={styles.sidebar}>
       <ModeSwitch/>
+      <Chat/>
     </div>
   </div>;
 }
