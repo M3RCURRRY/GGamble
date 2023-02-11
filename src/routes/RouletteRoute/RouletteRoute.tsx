6658dd63fdd1ea5dc8282833f72d9f4a6c99c482
M3RCURRRY <m3rcurrry1337@gmail.com>
2023-02-11 09:55:08 +0300

    Added Roulette component to route

--- src/routes/RouletteRoute/RouletteRoute.tsx
+++ src/routes/RouletteRoute/RouletteRoute.tsx
@@ -1,5 +1,6 @@
 import Chat from "../../components/Chat/Chat";
 import ModeSwitch from "../../components/ModeSwitch/ModeSwitch";
+import Roulette from "../../components/Roulette/Roulette";
 import styles from "./RouletteRoute.module.scss";
 
 const RouletteRoute = () => {
@@ -7,6 +8,9 @@ const RouletteRoute = () => {
     <div className={styles.sidebar}>
       <ModeSwitch/>
       <Chat/>
+    </div>
+    <div className={styles.roulette}>
+      <Roulette/>
     </div>
   </div>;
 }
