8a8317cc84cbd62c0944ad9b5729ba6bc7108091
M3RCURRRY <m3rcurrry1337@gmail.com>
2023-02-20 20:43:32 +0300

    Added Link routing

--- frontend/src/modules/ModeSwitch/ModeSwitch.tsx
+++ frontend/src/modules/ModeSwitch/ModeSwitch.tsx
@@ -2,12 +2,13 @@ import styles from "./ModeSwitch.module.scss";
 
 import roulette from "./../../assets/logos/roulette.png";
 import slots from "./../../assets/logos/slots.png";
+import { Link } from "react-router-dom";
 
 const ModeSwitch = () => {
   return (
     <div className={styles.modelist}>
-      <img src={roulette} alt="Roulette"/>
-      <img src={slots} alt="Slots"/>
+      <Link to={"/"}><img src={roulette} alt="Roulette"/></Link>
+      <Link to={"/slots"}><img src={slots} alt="Slots"/></Link>
       <img src={roulette} alt="Crash"/>
       <img src={slots} alt="Bets"/>
     </div>
